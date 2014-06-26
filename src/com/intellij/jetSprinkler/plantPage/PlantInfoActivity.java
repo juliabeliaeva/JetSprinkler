d979c29536a69de7c7db9da62e56c30fcb0c9934
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-06-27 02:47:05 +0400

    correct saving for plant data

--- src/com/intellij/jetSprinkler/plantPage/PlantInfoActivity.java
+++ src/com/intellij/jetSprinkler/plantPage/PlantInfoActivity.java
@@ -5,20 +5,23 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import com.intellij.jetSprinkler.R;
 import com.intellij.jetSprinkler.plantList.PlantListItem;
+
+import java.text.DateFormat;
 
 public class PlantInfoActivity extends Activity {
   private static final int REQUEST_IMAGE_CAPTURE = 1;
   public static final String PLANT_DATA = "plantData";
   private PlantListItem myData;
-  private EditText nameField;
+  private ImageView myImg;
+  private EditText myName;
+  private TextView myDate;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -27,42 +30,25 @@ public class PlantInfoActivity extends Activity {
 
     myData = (PlantListItem) getIntent().getExtras().get(PLANT_DATA);
 
-    final ImageView img = (ImageView) findViewById(R.id.imageView);
-    img.setImageBitmap(myData.getBitmap());
+    myImg = (ImageView) findViewById(R.id.imageView);
+    myName = (EditText) findViewById(R.id.plantName);
+    myDate = (TextView) findViewById(R.id.lastWatering);
+    Button btn = (Button) findViewById(R.id.button);
 
-    final Button btn = (Button) findViewById(R.id.button);
-    nameField = (EditText) findViewById(R.id.plantName);
-    nameField.setText(myData.getName());
-    nameField.addTextChangedListener(new TextWatcher() {
-                                       @Override
-                                       public void onTextChanged(CharSequence s, int start, int before, int count) {
-                                         btn.setEnabled(s.length() > 0);
-                                         if (s.length() > 0) {
-                                           myData.setName(s.toString());
-                                         }
-                                       }
-
-                                       @Override
-                                       public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                                       }
-
-                                       @Override
-                                       public void afterTextChanged(Editable s) {
-                                       }
-                                     }
-    );
+    updateInfo();
 
     btn.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         Intent result = new Intent();
+        myData.setName(myName.getText().toString());
         result.putExtra(PlantInfoActivity.PLANT_DATA, myData);
         setResult(Activity.RESULT_OK, result);
         finish();
       }
     });
 
-    img.setOnClickListener(new View.OnClickListener() {
+    myImg.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -79,6 +65,13 @@ public class PlantInfoActivity extends Activity {
       Bundle extras = data.getExtras();
       Bitmap imageBitmap = (Bitmap) extras.get("data");
       myData.setBitmap(imageBitmap);
+      updateInfo();
     }
+  }
+
+  private void updateInfo() {
+    myImg.setImageBitmap(myData.getBitmap());
+    myName.setText(myData.getName());
+    myDate.setText("Last watering on "+ DateFormat.getInstance().format(myData.getLastWatering()));
   }
 }
