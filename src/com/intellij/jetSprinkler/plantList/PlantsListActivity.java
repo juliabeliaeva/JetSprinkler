be1f8a26ecc072743558144cc92bd0e068632237
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-06-27 02:00:16 +0400

    PlantInfoActivity finished return values in activities

--- src/com/intellij/jetSprinkler/plantList/PlantsListActivity.java
+++ src/com/intellij/jetSprinkler/plantList/PlantsListActivity.java
@@ -1,25 +1,23 @@
 package com.intellij.jetSprinkler.plantList;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import com.intellij.jetSprinkler.R;
+import com.intellij.jetSprinkler.plantPage.PlantInfoActivity;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class PlantsListActivity extends Activity {
-  private static final int REQUEST_IMAGE_CAPTURE = 1;
   public static final String NAME = "NAME_";
   public static final String SIZE = "SIZE";
   private final ArrayList<PlantListItem> myPlants = new ArrayList<PlantListItem>();
+  public static final int MY_CHILD_ACTIVITY = 666;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -32,36 +30,37 @@ public class PlantsListActivity extends Activity {
             R.layout.plantlist_item_row, myPlants);
 
     ListView list = ((ListView) findViewById(R.id.listView));
-    Button btn = (Button) findViewById(R.id.add_button);
-
-    btn.setOnClickListener(new View.OnClickListener() {
+    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override
-      public void onClick(View v) {
-        AlertDialog.Builder alert = new AlertDialog.Builder(PlantsListActivity.this);
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        PlantListItem plant = myPlants.get(position);
 
-        alert.setTitle("New Plant Name");
-
-        // Set an EditText view to get user input
-        final EditText input = new EditText(PlantsListActivity.this);
-        alert.setView(input);
-
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-          public void onClick(DialogInterface dialog, int whichButton) {
-            String value = input.getText().toString();
-          }
-        });
-
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-          public void onClick(DialogInterface dialog, int whichButton) {
-
-          }
-        });
-
-        alert.show();
+        Intent i = new Intent(PlantsListActivity.this, PlantInfoActivity.class);
+        i.putExtra(PlantInfoActivity.PLANT_DATA, plant);
+        startActivityForResult(i, MY_CHILD_ACTIVITY);
       }
     });
 
     list.setAdapter(adapter);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    switch(requestCode) {
+      case (MY_CHILD_ACTIVITY) : {
+        if (resultCode == Activity.RESULT_OK) {
+          PlantListItem plant = ((PlantListItem) data.getExtras().get(PlantInfoActivity.PLANT_DATA));
+          for (int i=0;i<myPlants.size();i++){
+            if (myPlants.get(i).getNumber()==plant.getNumber()){
+              myPlants.set(i,plant);
+              break;
+            }
+          }
+        }
+        break;
+      }
+    }
   }
 
   @Override
@@ -90,13 +89,15 @@ public class PlantsListActivity extends Activity {
 
     for (int i = 0; i < plantsNumber; i++) {
       String name = sharedPreferences.getString(NAME + i, "" + i);
-      myPlants.add(new PlantListItem(i, name, null, false, new Date(10000)));
+      PlantListItem res = new PlantListItem(i);
+      res.setName(name);
+      myPlants.add(res);
     }
 
     if (myPlants.size() == 0) {
       // for testing purposes
-      myPlants.add(new PlantListItem(0, "Misha", null, false, new Date(10000)));
-      myPlants.add(new PlantListItem(0, "Misha2", null, false, new Date(10000)));
+      myPlants.add(new PlantListItem(0));
+      myPlants.add(new PlantListItem(1));
     }
   }
 
--- src/com/intellij/jetSprinkler/plantPage/PlantInfoActivity.java
+++ src/com/intellij/jetSprinkler/plantPage/PlantInfoActivity.java
@@ -2,35 +2,57 @@ package com.intellij.jetSprinkler.plantPage;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import com.intellij.jetSprinkler.R;
+import com.intellij.jetSprinkler.plantList.PlantListItem;
 
 public class PlantInfoActivity extends Activity {
+  private static final int REQUEST_IMAGE_CAPTURE = 1;
+  public static final String PLANT_DATA = "plantData";
+  private PlantListItem myData;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.plant_info);
 
-    Button btn = (Button) findViewById(R.id.add_button);
+    myData = (PlantListItem) getIntent().getExtras().get(PLANT_DATA);
 
+    ImageView img = (ImageView) findViewById(R.id.imageView);
+    Button btn = (Button) findViewById(R.id.button);
 
-//    btn.setOnClickListener(new View.OnClickListener() {});
-//
-//    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//      startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//    }
+    btn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent result = new Intent();
+        result.putExtra(PlantInfoActivity.PLANT_DATA, myData);
+        setResult(Activity.RESULT_OK, result);
+        finish();
+      }
+    });
 
-
-
+    img.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+          startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+      }
+    });
   }
+
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//      Bundle extras = data.getExtras();
-//      Bitmap imageBitmap = (Bitmap) extras.get("data");
-//      imageView.setImageBitmap(imageBitmap);
-//    }
+    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+      Bundle extras = data.getExtras();
+      Bitmap imageBitmap = (Bitmap) extras.get("data");
+      myData.setBitmap(imageBitmap);
+    }
   }
 }
