d3c825e639ea0a7c340d674413b4da3ff3bb927a
Michael Vlassiev <Michael.Vlassiev@jetbrains.com>
2014-06-27 02:17:48 +0400

    Start impilementing commands

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -9,23 +9,45 @@ void setup() {
   setup1302();
   setTime1302(0, 0, 23, 4, 26, 6, 2014);
   Serial.println("Press 1 to LED ON or 0 to LED OFF...");
+  Serial.setTimeout(10000);  // 10 seconds for typing
 }
 
 long tt = 0;
 
 void loop() {
-  if (Serial.available() > 0) {  //если пришли данные
-    int incomingByte = Serial.read(); // считываем байт
-    test1302();
-    tt = millis();
-    if(incomingByte == '0') {
-       digitalWrite(pinLED, LOW);  // если 1, то выключаем LED
-       Serial.println("LED OFF. Press 1 to LED ON!");  // и выводим обратно сообщение
+  if (Serial.available() > 0) {  // is there any command?
+    int cmd = Serial.read();     // read command
+    switch (cmd) {
+      case '0':
+        digitalWrite(pinLED, LOW);  // если 1, то выключаем LED
+        Serial.println("0LED OFF. Press 1 to LED ON!");  // и выводим обратно сообщение
+        break;
+      case '1':
+        digitalWrite(pinLED, HIGH); // если 0, то включаем LED
+        Serial.println("1LED ON. Press 0 to LED OFF!");
+        break;
+      case 'N':
+        Serial.print("N3\n");
+        break;
+      case 'V':
+        Serial.print("V0.1\n");
+        break;
+      case 'Y':
+        Serial.print("Y");
+        int n = Serial.parseInt();
+        digitalWrite(n, HIGH);
+        break;
+      case 'Z':
+        Serial.print("Z");
+        int n = Serial.parseInt();
+        digitalWrite(n, LOW);
+        break;
+        
+      default:
+        Serial.print(" ERROR CMD: '"); Serial.print(cmd); Serial.println("'");
+        break;
     }
-    if(incomingByte == '1') {
-       digitalWrite(pinLED, HIGH); // если 0, то включаем LED
-       Serial.println("LED ON. Press 0 to LED OFF!");
-    }
+    tt = millis()
   }
 
   if (millis() - tt > 5000) {
@@ -33,3 +55,4 @@ void loop() {
     tt = millis();
   }
 }
+
