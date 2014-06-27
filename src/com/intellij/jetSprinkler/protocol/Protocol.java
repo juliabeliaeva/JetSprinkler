c07452da01ab3763b620ec4035a16140c24af57d
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-06-27 06:46:08 +0400

    working on protocol implementation

---
+++ src/com/intellij/jetSprinkler/protocol/Protocol.java
@@ -0,0 +1,34 @@
+package com.intellij.jetSprinkler.protocol;
+
+import java.util.Date;
+
+public class Protocol {
+  public static String version(){
+
+  }
+
+  public static void setTime(Date date){
+
+  }
+
+  public static Date getDate(){
+
+  }
+
+  public static String getSensor(int id){
+
+  }
+
+  public static String getSprinklerCount(int id){
+
+  }
+
+  public static Timetable getTimetable(){
+
+  }
+
+  public static void setTimetable(Timetable t){
+
+  }
+
+}
---
+++ src/com/intellij/jetSprinkler/protocol/Timetable.java
@@ -0,0 +1,4 @@
+package com.intellij.jetSprinkler.protocol;
+
+public class Timetable {
+}
