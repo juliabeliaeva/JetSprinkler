853a93b8a7caf391dfe5000aaa03daea8d8d1e2b
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-06-28 04:55:06 +0400

    correct loading in timetable

--- src/com/intellij/jetSprinkler/connection/protocol/Timetable.java
+++ src/com/intellij/jetSprinkler/connection/protocol/Timetable.java
@@ -15,6 +15,7 @@ public class Timetable {
 
   public void load(String res) {
     items.clear();
+    if (res.equals("")) return;
     for (String s:res.split(";")){
       TimetableItem ti = new TimetableItem();
       ti.load(s);
@@ -35,10 +36,10 @@ public class Timetable {
     public void load(String res) {
       String[] params = res.split(":");
       if (params.length!=5) throw new IllegalArgumentException(res);
+      id = Byte.parseByte(params[0]);
       volume = Integer.parseInt(params[1]);
-      start = Integer.parseInt(params[1]);
-      period = Integer.parseInt(params[1]);
-      id = Byte.parseByte(params[1]);
+      start = Integer.parseInt(params[2]);
+      period = Integer.parseInt(params[3]);
     }
   }
 }
